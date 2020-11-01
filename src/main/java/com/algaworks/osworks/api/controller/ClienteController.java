da6498bf79fe836ca4dd1257cc53cbbbc8d2ce56
Fagner Souza <fagner.roberto89@gmail.com>
2020-11-01 15:18:26 -0300

    Updating 404 error code for client

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +24,12 @@ public class ClienteController {
 	}
 		
 		@GetMapping("/clientes/{clienteId}")
-		public Cliente buscar(@PathVariable Long clienteId) {
+		public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 			Optional<Cliente>cliente = clienteRepository.findById(clienteId);
 			
-			return cliente.orElse(null);
+			if (cliente.isPresent()) {
+				return ResponseEntity.ok(cliente.get());
+			}
+			return ResponseEntity.notFound().build();
 		}
 	}
