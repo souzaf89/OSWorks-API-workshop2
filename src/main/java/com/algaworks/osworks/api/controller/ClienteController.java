89f7fedb2862dc017feb05581c3ccff18dae283c
Fagner Souza <fagner.roberto89@gmail.com>
2020-11-01 14:09:20 -0300

    ClienteRepository JPA

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,30 +1,23 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setTelefone("19 33923212");
-		cliente1.setEmail("joaodascouves@algaworks.com");
+		return clienteRepository.findAll();
 		
-		var cliente2 = new Cliente();
-		cliente2.setId(1L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("19 33923212");
-		cliente2.setEmail("mariadasilva@algaworks.com");
-		
-		return Arrays.asList(cliente1, cliente2);
 	}
 }
