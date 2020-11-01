ac4b016167a3d1ec4ae4cf548fb853545c398151
Fagner Souza <fagner.roberto89@gmail.com>
2020-11-01 14:39:02 -0300

    findBy implementation

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,9 +1,11 @@
 package com.algaworks.osworks.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
@@ -18,6 +20,12 @@ public class ClienteController {
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
-		
 	}
-}
+		
+		@GetMapping("/clientes/{clienteId}")
+		public Cliente buscar(@PathVariable Long clienteId) {
+			Optional<Cliente>cliente = clienteRepository.findById(clienteId);
+			
+			return cliente.orElse(null);
+		}
+	}
