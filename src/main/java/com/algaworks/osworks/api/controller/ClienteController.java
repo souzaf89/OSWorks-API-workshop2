d4220e55b5ec2900a0b89e86463a6628b2d0a661
Fagner Souza <fagner.roberto89@gmail.com>
2020-11-01 16:07:25 -0300

    Endpoint Update Cliente

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -9,8 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,4 +46,18 @@ public class ClienteController {
 		public Cliente adicionar(@RequestBody Cliente cliente) {
 			return clienteRepository.save(cliente);
 		}
-	}
+		
+		@PutMapping("/{clienteId}")
+		public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+				@RequestBody Cliente cliente) {
+			
+			if (!clienteRepository.existsById(clienteId)) {
+				return ResponseEntity.notFound().build();
+			}
+			
+			cliente.setId(clienteId);
+			cliente = clienteRepository.save(cliente);
+			
+			return ResponseEntity.ok(cliente);
+	}	
+}
