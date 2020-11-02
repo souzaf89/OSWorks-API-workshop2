6ffa531b5a474f3e0dc33a21b42cdff7b7de1da7
Fagner Souza <fagner.roberto89@gmail.com>
2020-11-02 10:03:19 -0300

    handleExceptionInternal

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
+import com.algaworks.osworks.domain.service.CadastroClienteService;
 
 @RestController
 @RequestMapping("/clientes")
@@ -27,6 +28,9 @@ public class ClienteController {
 
 	@Autowired
 	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private CadastroClienteService cadastroCliente;
 	
 	@GetMapping
 	public List<Cliente> listar() {
@@ -46,7 +50,7 @@ public class ClienteController {
 		@PostMapping
 		@ResponseStatus(HttpStatus.CREATED)
 		public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
-			return clienteRepository.save(cliente);
+			return cadastroCliente.salvar(cliente);
 		}
 		
 		@PutMapping("/{clienteId}")
@@ -58,7 +62,7 @@ public class ClienteController {
 			}
 			
 			cliente.setId(clienteId);
-			cliente = clienteRepository.save(cliente);
+			cliente = cadastroCliente.salvar(cliente);
 			
 			return ResponseEntity.ok(cliente);
 	}	
@@ -69,7 +73,7 @@ public class ClienteController {
 				return ResponseEntity.notFound().build();
 		}
 			
-			clienteRepository.deleteById(clienteId);
+			cadastroCliente.excluir(clienteId);
 			
 			return ResponseEntity.noContent().build();
 		}
